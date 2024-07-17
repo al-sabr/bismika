a90c1e3c9ab502f95f55ed20f98cb65dc79c4e54
Ramazan Ergüder Bekrek <e.bekrek@yandex.com>
2024-07-17 21:39:25 +0100

    Compiling example but exits with app error.

--- include/modules/InMemorySchemaHandler.h
+++ include/modules/InMemorySchemaHandler.h
@@ -10,13 +10,39 @@
 
 class InMemorySchemaHandler : public saucer::module{
     public:
-    InMemorySchemaHandler(saucer::smartview_core *smartview, cppmicroservices::BundleContext ctx);
-    ~InMemorySchemaHandler();
-    void init(saucer::native::window *window, saucer::native::webview *webview) override;
-    void my_awesome_feature();
+    ~InMemorySchemaHandler(){
+
+    };
+    
+    InMemorySchemaHandler(saucer::smartview_core *smartview) : saucer::module(smartview){
+        
+    };
+
+    template <typename Serializer, typename... Modules>
+    InMemorySchemaHandler(saucer::smartview<Serializer, Modules...> *smartview) : saucer::module(smartview)
+    {
+        smartview->expose("module_add", [](int a, int b) { return a + b; });
+    }
+
+    void init(saucer::native::window *window, saucer::native::webview *webview) override
+    {
+        m_window  = window;
+        m_webview = webview;
+    }
+
+    void initBundleContext(cppmicroservices::BundleContext *ctx)
+    {
+        this->ctx = ctx;
+    }
+
+    void my_awesome_feature()
+    {
+        m_window->window->setWindowTitle("Hello from Module!");
+        m_webview->web_view->page()->setAudioMuted(true);
+    }
 
     private:
-    cppmicroservices::BundleContext ctx;
+    cppmicroservices::BundleContext *ctx;
     saucer::native::window *m_window;
     saucer::native::webview *m_webview;
 };
